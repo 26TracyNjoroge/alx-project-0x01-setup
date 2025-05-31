a7e2966ef7a963b417be58393263f504068ec6e0
Tracy Njoroge <tracywangari997@gmail.com>
2025-06-01 01:36:42 +0300

    Add user modal component

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -9,7 +9,7 @@ return (
       <Header />
       <main className="p-4"> 
         <div className="flex justify-between items-center mb-4">
-          <h1 className="text-2xl font-semibold text-gray-800">User Directory</h1>
+          <h1 className="text-2xl font-semibold text-red-800 bg-blue-600">User Directory</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
         </div>
 
