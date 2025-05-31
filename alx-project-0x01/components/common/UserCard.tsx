7b3d2c7050e179005da7684f8f9a56d12e1e4f9b
Tracy Njoroge <tracywangari997@gmail.com>
2025-06-01 00:07:34 +0300

    Update usecard component and users page

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,30 +1,10 @@
 import { UserProps } from "@/interfaces";
 
-const UserCard: React.FC<UserProps> = ({id,
-  name,
-  username,
-  email,
-  phone,
-  address }) => {
+const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
   return (
-    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <div className="mb-4">
-        <h2 className="text-2xl font-semibold text-gray-800">Username: @{username} {""} Id: {id}</h2>
-      </div>
-      <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
-        <p>
-          Name: {name}
-        </p>
-        <p>
-          Email: {email}
-        </p>
-        <p>
-          Phone: {phone}
-        </p>
-        <p>
-          Address: {address.city}
-        </p>
-      </div>
+    <div className="p-6 bg-red ">
+      <h2>{user.name}</h2>      
+      <p>@{user.username}</p> 
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,33 @@
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
+import { UsersPageProps } from "@/interfaces";
 
-async function User() {
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
-    <div className="p-4">
-        <Header />
-      <h1 className="text-xl font-bold mb-4">Posts</h1>
+    <div>
+      <Header />
+      <div className="flex justify-between">
+        <h2 className="">Users</h2>
+        <h2 className="">Add Users</h2>
+      </div>
+      <div className="grid grid-cols-3 gap-4 ">
+            {users.map((user: UserProps) => (
+              <UserCard key={user.id} user={user} /> 
+            ))}
+      </div>
     </div>
   );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const users = await response.json()
+
+  return {
+    props: {
+      users
+    }
+  }
 }
-export default User;
+export default Users;
